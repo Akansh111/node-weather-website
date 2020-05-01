bcae00375dc11230030734505bbf6d9dbcbf0003
Akansh Goel <akagoel2@publicisgroupe.net>
2020-05-01 13:14:27 +0530

    removed consoles

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('in app.js');
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
